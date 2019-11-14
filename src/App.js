44a19d54cff11baed4502b4e677b60eb3e1d19bd
malaka <blasthotcorn@protonmail.com>
2019-11-14 10:27:41 +0800

    a makeshift way of setting spinner

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import FormWord from './FormWord.js';
+import React, { useState, useEffect } from "react";
+import FormWord from "./FormWord.js";
 import Definitions from "./Definitions.js";
 //import logo from './logo.svg';
-import './App.css';
-console.log('hello');
+import "./App.css";
+console.log("hello");
 
 let App = () => {
-  const [definitions, setDefinitions] = useState([])
-  useEffect(()=>console.log('hi'))
-  const get_defs = function(word) {
-    fetch(`http://api.urbandictionary.com/v0/define?term=${word}`)
-    .then(data => data.json())
-    .then(res => setDefinitions(res.list))
-  }
+  const [definitions, setDefinitions] = useState([]);
+  const [spinner, setSpinner] = useState(false);
+  useEffect(() => {
+    console.log('hi');
+  });
 
+  const get_defs = function(word) {
+    setSpinner(true);
+    fetch(`http://api.urbandictionary.com/v0/define?term=${word}`)
+      .then(data => data.json())
+      .then(res => {
+        setDefinitions(res.list);
+        setSpinner(false);
+      });
+  };
 
   return (
     <div>
       <FormWord handleclick={get_defs} />
+      { spinner && <div>loading...</div>}
       <Definitions definitions={definitions}/>
     </div>
-  )
-
-}
+  );
+};
 
 export default App;
--- src/Definitions.js
+++ src/Definitions.js
@@ -2,17 +2,16 @@ import React from "react";
 
 function Definitions(props) {
     let definitions = props.definitions;
-    let error_m = 'what?'
     return (<> 
     {
-        definitions.length >= 1 ? definitions.map(definition => {
+        definitions.length > 0 ? definitions.map(definition => {
             return  (
                 <li key={definition.defid}>
                     <div>{definition.word}</div>
                     <div>{definition.definition}</div>
                 </li>
             )
-            }) : <div>{error_m}</div>
+            }) : <div></div>
     }
     </>)
     
